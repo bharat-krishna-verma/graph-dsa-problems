c4bb60a0724ac621dd82cb45eae65785bb72c41c
sparenamecooldude69 <108430804+sparenamecooldude69@users.noreply.github.com>
2025-08-15 18:01:08 +0530

    added bfs approach

--- number_of_provinces.java
+++ number_of_provinces.java
@@ -1,7 +1,9 @@
 //LeetCode- 547
 //We are given a adjacency matrix , we can solve it either by creating adjacency list of by 
 //adjacenecy matrix itself , traversal can be any -bfs-dfs
+import java.util.*;
 public class number_of_provinces {
+    //using dfs
     private void dfs(int[][]isConnected,int node,boolean [] vis){
         vis[node] =true;
         for(int i =0;i<isConnected.length;i++){
@@ -22,5 +24,31 @@ public class number_of_provinces {
         }
         return count;
     }
-    
+//using bfs
+public void bfs(int[][] isConnected,boolean [] vis,int start){
+    Queue<Integer> q = new LinkedList<>();
+    q.offer(start);
+    vis[start]= true;
+    while(!q.isEmpty()){
+        int curr = q.poll();
+        for(int i =0;i<isConnected.length;i++){
+            if(isConnected[curr][i]==1&&!vis[i]){
+                vis[i]=true;
+                q.offer(i);
+            }
+        }
+    }
+}
+ public int questionBFS(int[][] isConnected){
+    int count =0;
+    int n = isConnected.length;
+    boolean [] vis = new boolean[n];
+    for(int i =0;i<n;i++){
+        if(!vis[i]){
+            bfs(isConnected,vis,i);
+            count++;
+        }
+    }
+    return count;
+ }
 }
